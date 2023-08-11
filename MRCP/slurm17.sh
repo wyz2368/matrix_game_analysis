f26b4f221952733eb6da70ece805a2adba728946
wangyzh <wangyzh@umich.edu>
2023-08-11 14:56:02 -0400

    "add back MRCP calculator"

--- MRCP/slurm17.sh
+++ MRCP/slurm17.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=14g
-#SBATCH --time=10-00:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --account=wellman1
 #SBATCH --partition=standard
 
